eb63a1f7e5817f3668b265ca38c1ad56181f02dd
18721336535 <1186014040@qq.com>
2020-06-09 21:59:48 +0800

    first submit

---
+++ src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,42 @@
+package com.itheima.domain;
+
+public class User {
+
+    private Long USE_ID;
+    private String USE_NAME;
+    private String USE_SEX;
+    private String USE_AGE;
+
+
+    public Long getUSE_ID() {
+        return USE_ID;
+    }
+
+    public void setUSE_ID(Long USE_ID) {
+        this.USE_ID = USE_ID;
+    }
+
+    public String getUSE_NAME() {
+        return USE_NAME;
+    }
+
+    public void setUSE_NAME(String USE_NAME) {
+        this.USE_NAME = USE_NAME;
+    }
+
+    public String getUSE_SEX() {
+        return USE_SEX;
+    }
+
+    public void setUSE_SEX(String USE_SEX) {
+        this.USE_SEX = USE_SEX;
+    }
+
+    public String getUSE_AGE() {
+        return USE_AGE;
+    }
+
+    public void setUSE_AGE(String USE_AGE) {
+        this.USE_AGE = USE_AGE;
+    }
+}
