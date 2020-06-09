009a939358162e1e2987836e57716a95b28bfc5c
18721336535 <1186014040@qq.com>
2020-06-09 22:55:24 +0800

    log

--- src/main/java/com/itheima/controller/MybatisController.java
+++ src/main/java/com/itheima/controller/MybatisController.java
@@ -1,7 +1,10 @@
 package com.itheima.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.itheima.domain.User;
 import com.itheima.mapper.UserMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +18,13 @@ public class MybatisController {
     @Autowired
     private UserMapper userMapper;
 
+    private static final Logger logger = LoggerFactory.getLogger(MybatisController.class);
     @RequestMapping("/query")
     @ResponseBody
     public List<User> queryUserList(){
         List<User> users = userMapper.queryUserList();
+        logger.info("日志打印记录成功");
+        logger.info(JSON.toJSONString(users));
         return users;
     }
 
