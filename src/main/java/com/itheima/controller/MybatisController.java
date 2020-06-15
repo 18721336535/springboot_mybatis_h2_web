5e6eb33197dd2cc21ffb90de5723d6c4ebe60f97
18721336535 <1186014040@qq.com>
2020-06-16 00:07:34 +0800

    web

--- src/main/java/com/itheima/controller/MybatisController.java
+++ src/main/java/com/itheima/controller/MybatisController.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,50 +30,56 @@ public class MybatisController {
 
     @RequestMapping("/user/login")
     @ResponseBody
-    public void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public Map<String,Object> login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //1.获取用户名和密码数据
         Map<String, String[]> map = request.getParameterMap();
-        //2.封装User对象
-        User user = new User();
-        try {
-            BeanUtils.populate(user,map);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-
-        //3.调用Service查询
-        // UserService service = new UserServiceImpl();
-        User u  = new User();//service.login(user);
-
-        ResultInfo info = new ResultInfo();
-
-        //4.判断用户对象是否为null
-//        if(u == null){
-//            //用户名密码或错误
-//            info.setFlag(false);
-//            info.setErrorMsg("用户名密码或错误");
+        Map<String,Object> result = new HashMap<>();
+        result.put("flag",false);
+        System.out.println(JSON.toJSONString(map));
+        System.out.println(JSON.toJSONString(result));
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        return  result;
+//        //2.封装User对象
+//        User user = new User();
+//        try {
+//            BeanUtils.populate(user,map);
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (InvocationTargetException e) {
+//            e.printStackTrace();
 //        }
-        //5.判断用户是否激活
-//        if(u != null && !"Y".equals(u.getStatus())){
-//            //用户尚未激活
-//            info.setFlag(false);
-//            info.setErrorMsg("您尚未激活，请激活");
-//        }
-        //6.判断登录成功
-//        if(u != null && "Y".equals(u.getStatus())){
-            request.getSession().setAttribute("user",u);//登录成功标记
-
-            //登录成功
-            info.setFlag(true);
-//        }
-
-        //响应数据
-        ObjectMapper mapper = new ObjectMapper();
-
-        response.setContentType("application/json;charset=utf-8");
-        mapper.writeValue(response.getOutputStream(),info);
+//
+//        //3.调用Service查询
+//        // UserService service = new UserServiceImpl();
+//        User u  = new User();//service.login(user);
+//
+//        ResultInfo info = new ResultInfo();
+//
+//        //4.判断用户对象是否为null
+////        if(u == null){
+////            //用户名密码或错误
+////            info.setFlag(false);
+////            info.setErrorMsg("用户名密码或错误");
+////        }
+//        //5.判断用户是否激活
+////        if(u != null && !"Y".equals(u.getStatus())){
+////            //用户尚未激活
+////            info.setFlag(false);
+////            info.setErrorMsg("您尚未激活，请激活");
+////        }
+//        //6.判断登录成功
+////        if(u != null && "Y".equals(u.getStatus())){
+//            request.getSession().setAttribute("user",u);//登录成功标记
+//
+//            //登录成功
+//            info.setFlag(true);
+////        }
+//
+//        //响应数据
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        response.setContentType("application/json;charset=utf-8");
+//        mapper.writeValue(response.getOutputStream(),info);
     }
 
 
