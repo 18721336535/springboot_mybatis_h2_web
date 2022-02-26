ccee17c14663f178d567bd2a138d7a083d7cabe0
18721336535 <1186014040@qq.com>
2022-02-26 10:51:45 +0800

    job test

--- src/test/java/com/zbq/Test.java
+++ src/test/java/com/zbq/Test.java
@@ -1,10 +1,16 @@
 package com.zbq;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 //    var operations;
 //    Dictionary<String, Action> dc = new Dictionary<String, Action>() {
 //    }
     public static void main(String args[]){
-
+        List<String> bzlist = new ArrayList<String>();
+        while(true) {
+            bzlist.add("oomTest");
+        }
     }
 }
