89c6844563923253ef880e4261b23ce1f07138f6
18721336535 <1186014040@qq.com>
2022-03-21 14:01:37 +0800

    test job

--- threadPool/src/main/java/completableFuture/TestEntry.java
+++ threadPool/src/main/java/completableFuture/TestEntry.java
@@ -1,5 +1,9 @@
 package completableFuture;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -9,11 +13,21 @@ public class TestEntry implements Runnable{
     TestEntry(int i){
         pp = i;
     }
-    public static void main(String args[]){
+    public static void main(String args[]) throws ExecutionException, InterruptedException {
         int i =0;
-        ExecutorService exe= Executors.newFixedThreadPool(1);
-        exe.submit(new TestEntry(8));
-        exe.shutdown();
+//        ExecutorService exe= Executors.newFixedThreadPool(1);
+//        exe.submit(new TestEntry(8));
+//        exe.shutdown();
+        CompletableFuture<List<String>> f = CompletableFuture.supplyAsync(()->{
+            try {
+                Thread.sleep(10000);
+                System.out.println("hi andy");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return Arrays.asList("d","f","5");
+        }).thenApplyAsync((x)->{System.out.println("hello-");return new ArrayList<>();});
+        System.out.println(f.get());
         System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
     }
 
