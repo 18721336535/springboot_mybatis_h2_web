c9c101666b6cbeeaf8da8570a6a1f7bdc7feae0e
18721336535 <1186014040@qq.com>
2022-08-04 14:57:17 +0800

    some txt

--- doc_demo/docs/linux/common_commands.sh
+++ doc_demo/docs/linux/common_commands.sh
@@ -1,3 +1,3 @@
-#check the connections from the current host to target host.午物务
+#check the connections from the current host to target host.午物务 清兴趣
 telnet [targetHost] [port] //also able to get ip
 netstat -anl| grep ip | wc -l
